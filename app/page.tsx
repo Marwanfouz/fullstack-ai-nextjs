9dd8a5040bc1d8d7a25b1e514e55b4d2fddf32c8
Marwan Fouz <marwanfouz76@gmail.com>
2024-06-09 04:00:56 +0300

    db schema & create a new user

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
-import Link from "next/link";
+import { auth } from '@clerk/nextjs/server'
+import Link from 'next/link'
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth()
+  let href = userId ? '/journal' : '/new-user'
+
   return (
     <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
       <div className="w-full max-w-[600px] mx-auto">
@@ -9,10 +13,10 @@ export default function Home() {
           This is the best app for tracking your mood through out your life. All
           you have to do is be honest.
         </p>
-        <Link href='/journal'>
-        <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
-          get started
-        </button>
+        <Link href={href}>
+          <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
+            get started
+          </button>
         </Link>
       </div>
     </div>
